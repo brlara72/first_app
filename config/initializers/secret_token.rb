933be3ffeb61ccd4adc4682dbe5625466b83cd0f
U-FIVEMILECAPITAL\blara <blara@pc-blara.FiveMileCapital.com>
2012-06-06 16:38:57 -0400

    Initial GIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '5c3d58fb60283048c2f456f18ee3f3b7d7bc80e3682e2cf213ba19d8c61376963193456216c7f04613d58755829003083935233d001014e98b1aea442558e828'
